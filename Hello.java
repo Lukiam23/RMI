aeea472103e4fd9293eb2619bdee2cd26be514f7
Lukiam23 <matheus.1596@hot.com>
2016-08-23 13:39:11 -0300

    Delete Hello.java

--- Hello.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Hello extends Remote
-{
-	void sayHello(String text) throws RemoteException;
-	String enviarMensagem(String nome, String msg) throws RemoteException;
-}
