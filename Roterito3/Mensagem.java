e87a5cf03b2731cfb14df99af8e4f3bd1f818d53
Lukiam23 <matheus.1596@hot.com>
2016-09-10 18:10:47 -0300

    Update Mensagem.java

--- Roterito3/Mensagem.java
+++ Roterito3/Mensagem.java
@@ -2,13 +2,14 @@ import java.io.Serializable;
 
 public class Mensagem implements Serializable{
 	private int id,nsala;
-	private String msg,nome;
-	public Mensagem(int id,String msg, int nsala, String nome)
+	private String msg,nome,destino;
+	public Mensagem(int id,String msg, int nsala, String nome,String destino)
 	{
 		this.id = id;
 		this.msg = msg;
 		this.nsala = nsala;
 		this.nome = nome;
+		this.destino = destino;
 	}
 	
 	public int getId()
@@ -26,5 +27,9 @@ public class Mensagem implements Serializable{
 	public String getNome()
 	{
 		return this.nome;
+	}
+	public String getDest()
+	{
+		return this.destino;
 	}
 }
